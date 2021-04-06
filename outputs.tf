f60a09a565eb6801e1249a7a9936553eeb997407
Joseph Ligier <joseph.ligier@accenture.com>
2021-04-06 13:00:08 +0200

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
 output "kubeconfig" {
   value = huaweicloud_cce_cluster.this.kube_config_raw
 }
+
+output "url" {
+  value = huaweicloud_cce_cluster.this.internal
+}
+
+
+output "cluster_name" {
+  value = huaweicloud_cce_cluster.this.certificate_clusters/name
+}
